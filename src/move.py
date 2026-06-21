9df3d1b9a9004a6936aa8b1d00f6fd353cfca834
aidanahn <aidanahn08@gmail.com>
2026-06-21 17:13:54 -0400

    Convert Move to a dataclass

--- src/move.py
+++ src/move.py
@@ -1,9 +1,10 @@
+from dataclasses import dataclass
 from typing import Optional
 from pieces.piece import Piece
 
+@dataclass
 class Move:
-    def __init__(self, from_sq: tuple[int, int], to_sq: tuple[int, int], captured: Optional[Piece]=None, is_castling: bool=False):
-        self.from_sq = from_sq
-        self.to_sq = to_sq
-        self.captured = captured
-        self.is_castling = is_castling
+    from_sq: tuple[int, int]
+    to_sq: tuple[int, int]
+    captured: Optional[Piece] = None
+    is_castling: bool = False
